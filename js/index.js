a9c3f5931ad36cc1e39b63771ab46900f1661257
viniceosmr <miiller.vini@gmail.com>
2017-11-13 01:43:38 -0200

    Adicionando autenticação

---
+++ js/index.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+    var url = window.location.href;
+    if (url.lastIndexOf('#access_token=') != -1)
+        var token = url.substring(url.lastIndexOf('#access_token=')+14);
+
+    console.log(token);
+
+    if(typeof token!== 'undefined'){
+        $('#btnAutenticar').html('autenticado');
+
+        $.ajax({
+            url: 'https://api.instagram.com/v1/users/self/?access_token=' + token,
+            method: 'GET',
+            success: function (data) {
+                console.log(data);
+            }
+        });
+    }
+});
